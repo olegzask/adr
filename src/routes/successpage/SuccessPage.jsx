b3426169c5c497765e0b48794cce7ba90df55856
Oleg Zaskovets <olegzask@gmail.com>
2024-01-23 16:09:16 -0700

    fresh updates

--- src/routes/successpage/SuccessPage.jsx
+++ src/routes/successpage/SuccessPage.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import { Helmet } from "react-helmet";
 import SuccessWindow from "../../components/successWindow/SuccessWindow";
 import "./sucesspage.styles.css"
 
@@ -18,6 +19,20 @@ export default function SuccessPage() {
    
 return (
     <div className="success-page-ct">
+      <Helmet>
+        
+   {/* <!-- Google tag (gtag.js) --> */}
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QLHER9KTLW">
+</script>
+<script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QLHER9KTLW');
+          `}
+        </script>
+      </Helmet>
         <h2>SUCCESS! REQUEST RECEIVED!</h2>
         <SuccessWindow fields={{ handler: goBackPage, errHandler: setErrorBooking }} />
     </div>
