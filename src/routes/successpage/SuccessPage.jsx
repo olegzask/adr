eccf6da52ba1da84825c443f223f86066a90d3ea
Oleg Zaskovets <olegzask@gmail.com>
2024-01-31 21:15:04 -0700

    fresh updates

--- src/routes/successpage/SuccessPage.jsx
+++ src/routes/successpage/SuccessPage.jsx
@@ -9,6 +9,22 @@ export default function SuccessPage() {
   const [errorBooking, setErrorBooking] = useState(false);
 
 
+//   <Helmet>
+        
+//   {/* <!-- Google tag (gtag.js) --> */}
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-QLHER9KTLW">
+// </script>
+// <script>
+//          {`
+//            window.dataLayer = window.dataLayer || [];
+//            function gtag(){dataLayer.push(arguments);}
+//            gtag('js', new Date());
+//            gtag('config', 'G-QLHER9KTLW');
+//          `}
+//        </script>
+//      </Helmet>
+
+
   const goBackPage = ()=> {
     history.back();
     // window.location.href = "https://adrenalinaudio.ca"
@@ -19,20 +35,7 @@ export default function SuccessPage() {
    
 return (
     <div className="success-page-ct">
-      <Helmet>
-        
-   {/* <!-- Google tag (gtag.js) --> */}
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-QLHER9KTLW">
-</script>
-<script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-QLHER9KTLW');
-          `}
-        </script>
-      </Helmet>
+
         <h2>SUCCESS! REQUEST RECEIVED!</h2>
         <SuccessWindow fields={{ handler: goBackPage, errHandler: setErrorBooking }} />
     </div>
