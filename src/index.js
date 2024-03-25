a7e50dfec3e9703c2415fd4ffd06250ef2eb0338
Oleg Zaskovets <olegzask@gmail.com>
2024-03-25 12:56:52 -0600

    fresh updates

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { BooleanProvider } from "./store";
+import ReactDOM from 'react-dom';
 import App from "./App";
 import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(
-  <BrowserRouter>
+// root.render(
+//   <BrowserRouter>
+//     <BooleanProvider>
+//       <App />
+//     </BooleanProvider>
+//   </BrowserRouter>
+// );
+
+ReactDOM.hydrate(
+  <React.StrictMode>
+    <BrowserRouter>
     <BooleanProvider>
       <App />
     </BooleanProvider>
   </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
